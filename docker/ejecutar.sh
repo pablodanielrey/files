de8d2b3e4caf0dc2ae7d4e23f1051eae35002a9f
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-08-29 19:31:19 -0300

    agregue la instalacion del paquet

--- docker/ejecutar.sh
+++ docker/ejecutar.sh
@@ -1,1 +1,4 @@
-sudo docker run -ti -d --name files -v $(pwd)/src:/src -p 5010:5000 -p 5011:5001 -p 5012:5002 -p 5013:5003 --env-file /home/pablo/gitlab/fce/produccion/files files
+#!/bin/bash
+sudo docker run -ti -d --name files -v $(pwd)/src:/src -p 5010:5000 -p 5011:5001 -p 5012:5002 -p 5013:5003 --env-file $HOME/gitlab/fce/produccion/files files
+sudo docker exec -t files bash instalar.sh
+
